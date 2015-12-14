3ec7fe52d9dd4bd1365b6e832ab40576e09d7496
shammunhossain <sadmanhossain0088@live.com>
2015-12-14 10:13:20 +0700

    update 14dec15

--- Week2-2/js/object-literal.js
+++ Week2-2/js/object-literal.js
@@ -1,40 +1,51 @@
 var hotel = {
 
+
 name: 'SADMAN HOTEL',
 rooms: 40,
 booked: 25,
 checkAvailability : function() {
 return this.rooms - this.booked;
 }
+
 };
+
 
 var elName = document.getElementById('hotelName');
 elName.textContent = hotel.name;
 
+
 var elRooms = document.getElementById('rooms');
 elRooms.textContent = hotel.checkAvailability();
 
+
 var hotel = new Object();
+
 
 hotel.name = 'SADMAN HOTEL'
 hotel.rooms = 40;
 hotel.booked = 25;
 
+
 hotel.checkAvailability = function(){
    return this.rooms - this.booked;
 };
 
+
 function Hotel(name, rooms, booked){
+
 
 this.name = name;
 this.rooms = rooms;
 this.booked = booked;
+
 
 this.checkAvailability = function(){
   return this.rooms - this.booked;
 };
 
 }
+
 
 var sadmanHotel = new Hotel ('SADMAN HOTEL', 40, 25);
 
